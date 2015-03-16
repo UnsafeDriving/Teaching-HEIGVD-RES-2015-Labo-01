01e153f77966440b320b873afe62fa06d4d78a76
Mario Ferreira <mario.jorge.ferreira@gmail.com>
2015-03-16 02:31:21 +0100

    Fin laboratoire 1

--- Lab01App-build/Lab01App-test/src/test/java/ch/heigvd/res/lab01/impl/ApplicationTest.java
+++ Lab01App-build/Lab01App-test/src/test/java/ch/heigvd/res/lab01/impl/ApplicationTest.java
@@ -26,129 +26,115 @@ import org.junit.BeforeClass;
  */
 public class ApplicationTest {
 
-  private static final Logger LOG = Logger.getLogger(ApplicationTest.class.getName());
+    private static final Logger LOG = Logger.getLogger(ApplicationTest.class.getName());
 
-  private static int NUMBER_OF_QUOTES = 20;
+    private static int NUMBER_OF_QUOTES = 20;
 
-  @BeforeClass
-  public static void invokeApplication() throws IOException {
-    IApplication application = new Application();
-    application.fetchAndStoreQuotes(NUMBER_OF_QUOTES); 
-    application.processQuoteFiles();
-  }
-  
-  @Test
-  public void theAuthorMustProvideAnEmailAddress() {
-    IApplication application = new Application();
-    String email = application.getAuthorEmail();
-    assertNotNull(email);
-    assertTrue(email.indexOf("@heig-vd.ch") != -1);
-  }
-  
-  @Test
-  public void theApplicationShouldGenerateTheCorrectNumberOfQuoteFiles() {
-    String[] extensions = {"utf8"};
-    Collection<File> files = FileUtils.listFiles(new File(Application.WORKSPACE_DIRECTORY), extensions, true);
-    assertEquals(NUMBER_OF_QUOTES, files.size());
-  }
-  
-  @Test
-  public void theApplicationShouldUseTheCorrectSyntaxToNameTheQuoteFiles() {
-    String[] extensions = {"utf8"};
-    Collection<File> files = FileUtils.listFiles(new File(Application.WORKSPACE_DIRECTORY), extensions, true);
-    for (File file : files) {
-      String filename = file.getName();
-      Pattern pattern = Pattern.compile("quote-\\d*.utf8");
-      Matcher matcher = pattern.matcher(filename);
-      assertTrue(matcher.matches());
+    @BeforeClass
+    public static void invokeApplication() throws IOException {
+        IApplication application = new Application();
+        application.fetchAndStoreQuotes(NUMBER_OF_QUOTES);
+        application.processQuoteFiles();
     }
-  }
-  
-  @Test
-  public void theApplicationShouldGenerateTheCorrectNumberOfOutputFiles() {
-    String[] extensions = {"out"};
-    Collection<File> files = FileUtils.listFiles(new File(Application.WORKSPACE_DIRECTORY), new IOFileFilter() {
 
-      @Override
-      public boolean accept(File file) {
-        return file.getName().endsWith(".utf8.out");
-      }
+    @Test
+    public void theAuthorMustProvideAnEmailAddress() {
+        IApplication application = new Application();
+        String email = application.getAuthorEmail();
+        assertNotNull(email);
+        assertTrue(email.indexOf("@heig-vd.ch") != -1);
+    }
 
-      @Override
-      public boolean accept(File dir, String file) {
-        return file.endsWith(".utf8.out");
-      }
-    },TrueFileFilter.INSTANCE);
-    assertEquals(NUMBER_OF_QUOTES, files.size());
-  }
-  
-  @Test
-  public void theApplicationShouldBeAbleToGenerateTheListOfFileNames() throws IOException {
-    Application app = new Application();
-    app.clearOutputDirectory();
-    
-    String rootDirectory = Application.WORKSPACE_DIRECTORY;
-    FileUtils.deleteDirectory(new File(rootDirectory));
-    new File(rootDirectory).mkdir();
+    @Test
+    public void theApplicationShouldGenerateTheCorrectNumberOfQuoteFiles() {
+        String[] extensions = {"utf8"};
+        Collection<File> files = FileUtils.listFiles(new File(Application.WORKSPACE_DIRECTORY), extensions, true);
+        assertEquals(NUMBER_OF_QUOTES, files.size());
+    }
 
-    String dirA = rootDirectory + "/A";
-    String fileA1 = dirA + "/file1.txt";
-    String fileA2 = dirA + "/file2.txt";
-    String dirB = rootDirectory + "/A/B";
-    String dirC = rootDirectory + "/A/C";
-    String fileC1 = dirC + "/file1.txt";
-    String dirD = rootDirectory + "/A/C/D";
-    String fileD1 = dirD + "/file1.txt";
-    String fileD2 = dirD + "/file2.txt";
-    new File(dirA).mkdirs();
-    new File(dirB).mkdirs();
-    new File(dirC).mkdirs();
-    new File(dirD).mkdirs();
-    new File(fileA1).createNewFile();
-    new File(fileA2).createNewFile();
-    new File(fileC1).createNewFile();
-    new File(fileD1).createNewFile();
-    new File(fileD2).createNewFile();
-    
-    StringWriter writer = new StringWriter();
-    app.printFileNames(writer);
-    String output = writer.toString();
-    StringBuilder sb = new StringBuilder();
-    
-    sb.append(rootDirectory);
-    sb.append("\n");
-    sb.append(dirA);
-    sb.append("\n");
-    sb.append(fileA1);
-    sb.append("\n");
-    sb.append(fileA2);
-    sb.append("\n");
-    sb.append(dirB);
-    sb.append("\n");
-    sb.append(dirC);
-    sb.append("\n");
-    sb.append(fileC1);
-    sb.append("\n");
-    sb.append(dirD);
-    sb.append("\n");
-    sb.append(fileD1);
-    sb.append("\n");
-    sb.append(fileD2);
-    sb.append("\n");
-    
-    // Bug fix in the original unit test by teacher
-    Pattern p = Pattern.compile(File.separator);
-    Matcher m = p.matcher(output);
-    String outputWithUnixSeparator = m.replaceAll("/");
-    
-    String reference = sb.toString();
-    boolean applicationReturnsValidPlatformSpecificFilePaths = (reference.equals(output));
-    boolean applicationReturnsValidUnixFilePaths = (reference.equals(outputWithUnixSeparator));
-    boolean applicationReturnsValidFilePaths = (applicationReturnsValidPlatformSpecificFilePaths || applicationReturnsValidUnixFilePaths);
-    
-    assertTrue(applicationReturnsValidFilePaths);
+    @Test
+    public void theApplicationShouldUseTheCorrectSyntaxToNameTheQuoteFiles() {
+        String[] extensions = {"utf8"};
+        Collection<File> files = FileUtils.listFiles(new File(Application.WORKSPACE_DIRECTORY), extensions, true);
+        for (File file : files) {
+            String filename = file.getName();
+            Pattern pattern = Pattern.compile("quote-\\d*.utf8");
+            Matcher matcher = pattern.matcher(filename);
+            assertTrue(matcher.matches());
+        }
+    }
 
-  }
-  
+    @Test
+    public void theApplicationShouldGenerateTheCorrectNumberOfOutputFiles() {
+        String[] extensions = {"out"};
+        Collection<File> files = FileUtils.listFiles(new File(Application.WORKSPACE_DIRECTORY), new IOFileFilter() {
 
+            @Override
+            public boolean accept(File file) {
+                return file.getName().endsWith(".utf8.out");
+            }
+
+            @Override
+            public boolean accept(File dir, String file) {
+                return file.endsWith(".utf8.out");
+            }
+        }, TrueFileFilter.INSTANCE);
+        assertEquals(NUMBER_OF_QUOTES, files.size());
+    }
+
+    @Test
+    public void theApplicationShouldBeAbleToGenerateTheListOfFileNames() throws IOException {
+        Application app = new Application();
+        app.clearOutputDirectory();
+
+        String rootDirectory = Application.WORKSPACE_DIRECTORY;
+        FileUtils.deleteDirectory(new File(rootDirectory));
+        new File(rootDirectory).mkdir();
+
+        String dirA = rootDirectory + "\\A";
+        String fileA1 = dirA + "\\file1.txt";
+        String fileA2 = dirA + "\\file2.txt";
+        String dirB = rootDirectory + "\\A\\B";
+        String dirC = rootDirectory + "\\A\\C";
+        String fileC1 = dirC + "\\file1.txt";
+        String dirD = rootDirectory + "\\A\\C\\D";
+        String fileD1 = dirD + "\\file1.txt";
+        String fileD2 = dirD + "\\file2.txt";
+        new File(dirA).mkdirs();
+        new File(dirB).mkdirs();
+        new File(dirC).mkdirs();
+        new File(dirD).mkdirs();
+        new File(fileA1).createNewFile();
+        new File(fileA2).createNewFile();
+        new File(fileC1).createNewFile();
+        new File(fileD1).createNewFile();
+        new File(fileD2).createNewFile();
+
+        StringWriter writer = new StringWriter();
+        app.printFileNames(writer);
+        String output = writer.toString();
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(rootDirectory);
+        sb.append("\n");
+        sb.append(dirA);
+        sb.append("\n");
+        sb.append(fileA1);
+        sb.append("\n");
+        sb.append(fileA2);
+        sb.append("\n");
+        sb.append(dirB);
+        sb.append("\n");
+        sb.append(dirC);
+        sb.append("\n");
+        sb.append(fileC1);
+        sb.append("\n");
+        sb.append(dirD);
+        sb.append("\n");
+        sb.append(fileD1);
+        sb.append("\n");
+        sb.append(fileD2);
+        sb.append("\n");
+        assertEquals(sb.toString(), output);
+    }
 }
